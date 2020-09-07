9aeeb6cf45023d60a5b43cd522035b4ad5b9af57
류재상 <rjs9611@naver.com>
2020-09-07 21:03:01 +0900

    feat : Add group, apply (#46)
    * feat: Add group, apply model
    
    * fix: one to many relation
    
    * feat: resolve conversation
    
    Co-authored-by: GunHee Jang <30435522+GunHee5719@users.noreply.github.com>

--- mycomm/models/group.py
+++ mycomm/models/group.py
@@ -1,5 +1,36 @@
 from django.db import models
 
+from mycomm.models.department import Department
+from mycomm.models.user import User
+
 
 class Group(models.Model):
-    pass
+    name = models.CharField(
+        verbose_name="이름",
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    description = models.CharField(
+        verbose_name='내용',
+        max_length=255,
+        null=False,
+        blank=True
+    )
+
+    department = models.ForeignKey(
+        verbose_name='학과',
+        to=Department,
+        on_delete=models.CASCADE,
+        null=False,
+        related_name='department_group'
+    )
+
+    manager = models.ForeignKey(
+        verbose_name='관리자',
+        to=User,
+        on_delete=models.SET_NULL,
+        null=False,
+        related_name='manager_group'
+    )
